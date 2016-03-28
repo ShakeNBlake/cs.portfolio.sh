ef6c17df4097d3c3fe92f6ea042f29707eebd066
shakenblake <bhuynh.sh@mysummitps.org>
2016-03-28 22:25:21 +0000

    update

---
+++ Animation.js
@@ -0,0 +1,7 @@
+<button type="button"
+onclick="">
+Click me to turn left</button>
+
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to turn left</button>
